438bca8b0c00616b5af7a570d06a7b65d7634da5
victorloo <ila9970@gmail.com>
2018-04-15 05:14:28 +0800

    Posts can new, create and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: :asc)
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "You Created a Post!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
