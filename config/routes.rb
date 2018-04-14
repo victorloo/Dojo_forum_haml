8987d668e00fef74fb7878155403ed8487f3d32d
victorloo <ila9970@gmail.com>
2018-04-15 05:46:50 +0800

    setup admin for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   root 'posts#index'
+
+  namespace :admin do
+    resources :users
+    root 'users#index'
+  end
 end
