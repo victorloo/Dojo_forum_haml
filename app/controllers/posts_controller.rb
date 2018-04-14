04fa9d78d69393ce48b023b6f41ab5d905e63286
victorloo <ila9970@gmail.com>
2018-04-15 05:39:27 +0800

    Added authenticate_user! to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @posts = Post.order(id: :asc)
