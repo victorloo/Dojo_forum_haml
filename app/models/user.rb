258b95ff92fe14d52ce4963a0537b22538754662
victorloo <ila9970@gmail.com>
2018-04-15 17:51:50 +0800

    Added user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true
   has_many :posts
 
   def admin?
