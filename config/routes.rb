1216e216680c0a8705be154fcfa89376da2d0f21
victorloo <ila9970@gmail.com>
2018-04-15 14:59:31 +0800

    setup route and link for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
 
+  resources :users, only: [:show, :edit, :update]
+
   namespace :admin do
     resources :users
     root 'users#index'
