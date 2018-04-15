aa426cc2280f3cca07976bd85c90b2028cbd5249
victorloo <ila9970@gmail.com>
2018-04-15 18:07:34 +0800

    Added AvatarUploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+  
 end
