3b6edfee75c0960100a1531fdebdb52af103559d
victorloo <ila9970@gmail.com>
2018-04-15 17:54:58 +0800

    Added column image to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415070021) do
+ActiveRecord::Schema.define(version: 20180415095306) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180415070021) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
