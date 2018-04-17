27981f84951fea9aa6d4c57c3e2b6f383bbe41d2
victorloo <ila9970@gmail.com>
2018-04-17 16:33:32 +0800

    Created Jointable folder for category and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417081130) do
+ActiveRecord::Schema.define(version: 20180417082557) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "folders", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_folders_on_category_id"
+    t.index ["post_id"], name: "index_folders_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
