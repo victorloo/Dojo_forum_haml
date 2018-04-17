006eaf7791973cd712ecacac9f262a6d0af40d3b
victorloo <ila9970@gmail.com>
2018-04-17 23:40:40 +0800

    show errors message on form when validation fails

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   before_action :authenticate_admin
 
   def index
-    @categories = Category.order(id: :asc).page(params[:page]).per(10)
+    @categories = Category.order(id: :asc)
     @category = Category.new
   end
 
