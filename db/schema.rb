b3193590431b3f0751fa430474f1292068885f16
victorloo <ila9970@gmail.com>
2018-04-17 16:12:45 +0800

    Created model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415095306) do
+ActiveRecord::Schema.define(version: 20180417081130) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
