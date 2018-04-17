2fb7977193288e203da8e2045d7adacd8695c638
victorloo <ila9970@gmail.com>
2018-04-17 23:03:46 +0800

    Added category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+
+  def index
+    @categories = Category.order(id: :asc).page(params[:page]).per(10)
+  end
 end
