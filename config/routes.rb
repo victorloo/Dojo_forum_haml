07689809a99d82f37ac693d50452d2597e292a4e
victorloo <ila9970@gmail.com>
2018-04-19 03:09:06 +0800

    Setup Comment resources for normal users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
   root 'posts#index'
   resources :categories, only: :show
 
