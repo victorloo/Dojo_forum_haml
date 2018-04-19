652664b8facd63ce7d67ccc5b023ab9371172ec9
victorloo <ila9970@gmail.com>
2018-04-20 00:39:30 +0800

    Generated model View

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419163521) do
+ActiveRecord::Schema.define(version: 20180419163758) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 20180419163521) do
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_views_on_post_id"
+    t.index ["user_id"], name: "index_views_on_user_id"
   end
 
 end
