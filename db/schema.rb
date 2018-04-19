bad0ce8bed8fa5611f813eab0b07a78fc72d4494
victorloo <ila9970@gmail.com>
2018-04-20 01:23:57 +0800

    Added column comments_count to table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419163758) do
+ActiveRecord::Schema.define(version: 20180419172146) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180419163758) do
     t.string "image"
     t.string "privacy"
     t.integer "views_count", default: 0
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
