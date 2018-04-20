2e91b2edb5ff039e1f9745dfa0d48aa44d6dde78
victorloo <ila9970@gmail.com>
2018-04-20 23:29:47 +0800

    Added attributes comments_count to users table and setup counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419193742) do
+ActiveRecord::Schema.define(version: 20180420152611) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180419193742) do
     t.string "name"
     t.text "intro"
     t.string "avatar"
+    t.integer "comments_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
