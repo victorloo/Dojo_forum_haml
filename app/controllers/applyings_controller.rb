065aa1d2c22f239b8709adf9ff226102abe5672c
victorloo <ila9970@gmail.com>
2018-04-27 04:21:12 +0800

    Add create action for applying record

--- app/controllers/applyings_controller.rb
+++ app/controllers/applyings_controller.rb
@@ -1,2 +1,13 @@
 class ApplyingsController < ApplicationController
+  def create
+    @applying = current_user.applyings.build(target_id: params[:target_id])
+
+    if @applying.save
+      flash[:notice] = "已送出邀請"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @applying.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
