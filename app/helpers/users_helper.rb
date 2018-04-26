39e6260addf7e7f6ecbf84eafab4db5d60ef1b89
victorloo <ila9970@gmail.com>
2018-04-27 06:26:19 +0800

    According to friend(accept, not yet or ignore), Switch right state on page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,20 @@ module UsersHelper
     be_ignore_friends = self.inverse_nobodys
     all_friends = (target_friends + be_ignore_friends).uniq.sort
   end
+
+  def friend?(user)
+    self.friends.include?(user)
+  end
+
+  def nobody?(user)
+    self.nobodys.include?(user)
+  end
+
+  def target?(user)
+    self.targets.include?(user)
+  end
+
+  def inverse_nobodys?(user)
+    self.inverse_nobodys.include?(user)
+  end
 end
