4609f5e34356fca6408cbd5813407d24288a57d8
victorloo <ila9970@gmail.com>
2018-04-28 19:45:17 +0800

    Added index action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::PostsController < ApiController
+  def index
+    published = Post.all.where(status: "published")
+    @posts = published.all
+    render json: {
+      data: @posts.map do |post|
+        {
+          title: post.title,
+          content: post.content,
+          author: post.user.name
+        }
+      end
+    }
+  end
+  
 end
