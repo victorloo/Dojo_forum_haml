fe65ba9a00c526033b7d662461889e2b5a69abc5
victorloo <ila9970@gmail.com>
2018-04-28 19:25:30 +0800

    Setup routes for api/v1/posts

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:show, :new, :edit]
     root 'categories#index'
   end
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
